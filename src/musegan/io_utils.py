a281668b16bdd3373bacc69cd65290de7f86da51
Eric Xiao <erxiao@ucsd.edu>
2021-07-24 22:00:31 -0700

    fix for tempo param

--- src/musegan/io_utils.py
+++ src/musegan/io_utils.py
@@ -265,5 +265,5 @@ def save_pianoroll(filename, pianoroll, programs, is_drums, tempo,
 
     # Create and save the multitrack
     multitrack = pypianoroll.Multitrack(
-        tracks=tracks, tempo=tempo, resolution=beat_resolution)
+        tracks=tracks, tempo=np.array([tempo]), resolution=beat_resolution)
     multitrack.save(filename)
