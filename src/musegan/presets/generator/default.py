51b488d47700b77573ba0f08e39b0a5b9f854d38
Eric Xiao <erxiao@ucsd.edu>
2021-07-24 20:34:30 -0700

    trying again

--- src/musegan/presets/generator/default.py
+++ src/musegan/presets/generator/default.py
@@ -1,7 +1,7 @@
 """This file defines the network architecture for the generator."""
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
-from tf.nn import relu, leaky_relu, tanh, sigmoid
+from tensorflow.compat.v1.nn import relu, leaky_relu, tanh, sigmoid
 from ops import tconv3d, get_normalization
 
 NORMALIZATION = 'batch_norm' # 'batch_norm', 'layer_norm'
