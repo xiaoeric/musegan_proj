feeaa66eaa0a04ec54ae7295e8f09d4961ecd94e
Colin Lemarchand <53798233+friedsoap7@users.noreply.github.com>
2021-07-28 12:59:15 -0700

    might be garbage

--- src/musegan/presets/discriminator/default.py
+++ src/musegan/presets/discriminator/default.py
@@ -27,39 +27,39 @@ class Discriminator:
                 tensor_in, (-1, h.get_shape()[1], n_beats, self.beat_resolution,
                             h.get_shape()[3], h.get_shape()[4]))
             summed = tf.reduce_sum(reshaped, 3)
-            factor = int(h.get_shape()[3]) // 12
-            remainder = int(h.get_shape()[3]) % 12
+            factor = int(h.get_shape()[3]) // 4
+            remainder = int(h.get_shape()[3]) % 4
             reshaped = tf.reshape(
-                summed[..., :(factor * 12), :],
-                (-1, h.get_shape()[1], n_beats, factor, 12, h.get_shape()[4]))
-            chroma = tf.reduce_sum(reshaped, 3)                      # 4, 4, 12
+                summed[..., :(factor * 4), :],
+                (-1, h.get_shape()[1], n_beats, factor, 4, h.get_shape()[4]))
+            chroma = tf.reduce_sum(reshaped, 3)                      # 4, 4, 4
             if remainder:
                 chroma += summed[..., -remainder:, :]
 
             # Compute onset/offset feature
             padded = tf.pad(tensor_in[:, :, :-1],
                             ((0, 0), (0, 0), (1, 0), (0, 0), (0, 0)))
-            on_off_set = tf.reduce_sum(tensor_in - padded, 3, True)  # 4, 48, 1
+            on_off_set = tf.reduce_sum(tensor_in - padded, 3, True)  # 4, 16, 1
 
             # Pitch-time private network
             with tf.variable_scope('pitch_time_private'):
-                s1 = [conv_layer(h, 16, (1, 1, 12), (1, 1, 12))      # 4, 48, 7
+                s1 = [conv_layer(h, 16, (1, 1, 12), (1, 1, 12))      # 4, 16, 7
                       for _ in range(self.n_tracks)]
-                s1 = [conv_layer(s1[i], 32, (1, 3, 1), (1, 3, 1))    # 4, 16, 7
+                s1 = [conv_layer(s1[i], 32, (1, 4, 1), (1, 4, 1))    # 4, 4, 7
                       for i in range(self.n_tracks)]
 
             # Time-pitch private network
             with tf.variable_scope('time_pitch_private'):
-                s2 = [conv_layer(h, 16, (1, 3, 1), (1, 3, 1))        # 4, 16, 84
+                s2 = [conv_layer(h, 16, (1, 4, 1), (1, 4, 1))        # 4, 4, 84
                       for _ in range(self.n_tracks)]
-                s2 = [conv_layer(s2[i], 32, (1, 1, 12), (1, 1, 12))  # 4, 16, 7
+                s2 = [conv_layer(s2[i], 32, (1, 1, 12), (1, 1, 12))  # 4, 4, 7
                       for i in range(self.n_tracks)]
 
             h = [tf.concat((s1[i], s2[i]), -1) for i in range(self.n_tracks)]
 
             # Merged private network
             with tf.variable_scope('merged_private'):
-                h = [conv_layer(h[i], 64, (1, 1, 1), (1, 1, 1))      # 4, 16, 7
+                h = [conv_layer(h[i], 64, (1, 1, 1), (1, 1, 1))      # 4, 4, 7
                      for i in range(self.n_tracks)]
 
             h = tf.concat(h, -1)
@@ -76,9 +76,9 @@ class Discriminator:
 
             # Onset/offset stream
             with tf.variable_scope('on_off_set'):
-                o = conv_layer(on_off_set, 16, (1, 3, 1), (1, 3, 1)) # 4, 16, 1
-                o = conv_layer(o, 32, (1, 4, 1), (1, 4, 1))          # 4, 4, 1
-                o = conv_layer(o, 64, (1, 4, 1), (1, 4, 1))          # 4, 1, 1
+                o = conv_layer(on_off_set, 16, (1, 4, 1), (1, 4, 1)) # 4, 4, 1
+                o = conv_layer(o, 32, (1, 2, 1), (1, 2, 1))          # 4, 2, 1
+                o = conv_layer(o, 64, (1, 2, 1), (1, 2, 1))          # 4, 1, 1
 
             h = tf.concat((h, c, o), -1)
 
